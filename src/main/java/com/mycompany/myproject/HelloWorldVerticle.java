494479884140fcda8987d8d843228ea16b1b6563
Satoshi TAKIDA <pisatoshi@gmail.com>
2014-07-27 01:07:53 +0900

    add http handler verticle.

---
+++ src/main/java/com/mycompany/myproject/HelloWorldVerticle.java
@@ -0,0 +1,18 @@
+package com.mycompany.myproject;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.platform.Verticle;
+
+public class HelloWorldVerticle extends Verticle {
+
+  public void start() {
+    container.logger().info("Verticle start.");
+    vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+      public void handle(HttpServerRequest req) {
+        req.response().headers().set("Content-Type", "text/plain");
+        req.response().end("Hello World!");
+      }
+    }).listen(8080);
+  }
+}
